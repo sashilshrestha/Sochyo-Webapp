3c8c13293764f20600579079072f947c694526c3
sashilshrestha <sashilstha5@gmail.com>
2021-07-10 22:44:59 +0545

    Footer Management

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -8,38 +9,17 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+
 ?>
+<?php get_template_part('sidebar-templates/sidebar', 'footerfull'); ?>
 
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+<center>
+	<h5 class="uk-text-bolder uk-background-muted uk-padding-small">Sochyo.com.np</h5>
+</center>
 
-<div class="wrapper" id="wrapper-footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
-
-		<div class="row">
-
-			<div class="col-md-12">
-
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-						<?php understrap_site_info(); ?>
-
-					</div><!-- .site-info -->
-
-				</footer><!-- #colophon -->
-
-			</div><!--col end -->
-
-		</div><!-- row end -->
-
-	</div><!-- container end -->
-
-</div><!-- wrapper end -->
 
 </div><!-- #page we need this extra closing tag here -->
 
@@ -48,4 +28,3 @@ $container = get_theme_mod( 'understrap_container_type' );
 </body>
 
 </html>
-
