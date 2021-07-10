0a5904e19f8169587dc760fcfcf114797d898646
sashilshrestha <sashilstha5@gmail.com>
2021-07-10 22:08:52 +0545

    Uikits Added

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 // Defining requirements
-var gulp = require( 'gulp' );
-var plumber = require( 'gulp-plumber' );
-var sass = require( 'gulp-sass' );
-var babel = require( 'gulp-babel' );
-var postcss = require( 'gulp-postcss' );
-var rename = require( 'gulp-rename' );
-var concat = require( 'gulp-concat' );
-var uglify = require( 'gulp-uglify' );
-var imagemin = require( 'gulp-imagemin' );
-var sourcemaps = require( 'gulp-sourcemaps' );
-var browserSync = require( 'browser-sync' ).create();
-var del = require( 'del' );
-var cleanCSS = require( 'gulp-clean-css' );
-var autoprefixer = require( 'autoprefixer' );
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+var babel = require("gulp-babel");
+var postcss = require("gulp-postcss");
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var imagemin = require("gulp-imagemin");
+var sourcemaps = require("gulp-sourcemaps");
+var browserSync = require("browser-sync").create();
+var del = require("del");
+var cleanCSS = require("gulp-clean-css");
+var autoprefixer = require("autoprefixer");
 
 // Configuration file to keep your code DRY
-var cfg = require( './gulpconfig.json' );
+var cfg = require("./gulpconfig.json");
 var paths = cfg.paths;
 
 /**
@@ -23,44 +23,44 @@ var paths = cfg.paths;
  *
  * Run: gulp sass
  */
-gulp.task( 'sass', function() {
+gulp.task("sass", function () {
 	return gulp
-		.src( paths.sass + '/*.scss' )
+		.src(paths.sass + "/*.scss")
 		.pipe(
-			plumber( {
-				errorHandler( err ) {
-					console.log( err );
-					this.emit( 'end' );
+			plumber({
+				errorHandler(err) {
+					console.log(err);
+					this.emit("end");
 				},
-			} )
+			})
 		)
-		.pipe( sourcemaps.init( { loadMaps: true } ) )
-		.pipe( sass( { errLogToConsole: true } ) )
-		.pipe( postcss( [ autoprefixer() ] ) )
-		.pipe( sourcemaps.write( undefined, { sourceRoot: null } ) )
-		.pipe( gulp.dest( paths.css ) );
-} );
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(sass({ errLogToConsole: true }))
+		.pipe(postcss([autoprefixer()]))
+		.pipe(sourcemaps.write(undefined, { sourceRoot: null }))
+		.pipe(gulp.dest(paths.css));
+});
 
 /**
  * Optimizes images and copies images from src to dest.
  *
  * Run: gulp imagemin
  */
-gulp.task( 'imagemin', () =>
+gulp.task("imagemin", () =>
 	gulp
-		.src( paths.imgsrc + '/**' )
+		.src(paths.imgsrc + "/**")
 		.pipe(
 			imagemin(
 				[
 					// Bundled plugins
-					imagemin.gifsicle( {
+					imagemin.gifsicle({
 						interlaced: true,
 						optimizationLevel: 3,
-					} ),
-					imagemin.mozjpeg( {
+					}),
+					imagemin.mozjpeg({
 						quality: 100,
 						progressive: true,
-					} ),
+					}),
 					imagemin.optipng(),
 					imagemin.svgo(),
 				],
@@ -69,7 +69,7 @@ gulp.task( 'imagemin', () =>
 				}
 			)
 		)
-		.pipe( gulp.dest( paths.img ) )
+		.pipe(gulp.dest(paths.img))
 );
 
 /**
@@ -77,223 +77,221 @@ gulp.task( 'imagemin', () =>
  *
  * Run: gulp minifycss
  */
-gulp.task( 'minifycss', function() {
+gulp.task("minifycss", function () {
 	return gulp
-		.src( [
-			paths.css + '/custom-editor-style.css',
-			paths.css + '/theme.css',
-		] )
+		.src([paths.css + "/custom-editor-style.css", paths.css + "/theme.css"])
 		.pipe(
-			sourcemaps.init( {
+			sourcemaps.init({
 				loadMaps: true,
-			} )
+			})
 		)
 		.pipe(
-			cleanCSS( {
-				compatibility: '*',
-			} )
+			cleanCSS({
+				compatibility: "*",
+			})
 		)
 		.pipe(
-			plumber( {
-				errorHandler( err ) {
-					console.log( err );
-					this.emit( 'end' );
+			plumber({
+				errorHandler(err) {
+					console.log(err);
+					this.emit("end");
 				},
-			} )
+			})
 		)
-		.pipe( rename( { suffix: '.min' } ) )
-		.pipe( sourcemaps.write( './' ) )
-		.pipe( gulp.dest( paths.css ) );
-} );
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(sourcemaps.write("./"))
+		.pipe(gulp.dest(paths.css));
+});
 
 /**
  * Delete minified CSS files and their maps
  *
  * Run: gulp cleancss
  */
-gulp.task( 'cleancss', function() {
-	return del( paths.css + '/*.min.css*' );
-} );
+gulp.task("cleancss", function () {
+	return del(paths.css + "/*.min.css*");
+});
 
 /**
  * Compiles .scss to .css minifies css files.
  *
  * Run: gulp styles
  */
-gulp.task( 'styles', function( callback ) {
-	gulp.series( 'sass', 'minifycss' )( callback );
-} );
+gulp.task("styles", function (callback) {
+	gulp.series("sass", "minifycss")(callback);
+});
 
 /**
  * Watches .scss, .js and image files for changes.
  * On change re-runs corresponding build task.
- * 
+ *
  * Run: gulp watch
  */
-gulp.task( 'watch', function() {
+gulp.task("watch", function () {
 	gulp.watch(
-		[ paths.sass + '/**/*.scss', paths.sass + '/*.scss' ],
-		gulp.series( 'styles' )
+		[paths.sass + "/**/*.scss", paths.sass + "/*.scss"],
+		gulp.series("styles")
 	);
 	gulp.watch(
 		[
-			paths.dev + '/js/**/*.js',
-			'js/**/*.js',
-			'!js/theme.js',
-			'!js/theme.min.js',
+			paths.dev + "/js/**/*.js",
+			"js/**/*.js",
+			"!js/theme.js",
+			"!js/theme.min.js",
 		],
-		gulp.series( 'scripts' )
+		gulp.series("scripts")
 	);
 
 	// Inside the watch task.
-	gulp.watch( paths.imgsrc + '/**', gulp.series( 'imagemin-watch' ) );
-} );
+	gulp.watch(paths.imgsrc + "/**", gulp.series("imagemin-watch"));
+});
 
 /**
  * Starts browser-sync task for starting the server.
  *
  * Run: gulp browser-sync
  */
-gulp.task( 'browser-sync', function() {
-	browserSync.init( cfg.browserSyncOptions );
-} );
+gulp.task("browser-sync", function () {
+	browserSync.init(cfg.browserSyncOptions);
+});
 
 /**
  * Ensures the 'imagemin' task is complete before reloading browsers
  */
 gulp.task(
-	'imagemin-watch',
-	gulp.series( 'imagemin', function() {
+	"imagemin-watch",
+	gulp.series("imagemin", function () {
 		browserSync.reload();
-	} )
+	})
 );
 
 /**
  * Starts watcher with browser-sync.
  * Browser-sync reloads page automatically on your browser.
- * 
+ *
  * Run: gulp watch-bs
  */
-gulp.task( 'watch-bs', gulp.parallel( 'browser-sync', 'watch' ) );
+gulp.task("watch-bs", gulp.parallel("browser-sync", "watch"));
 
 // Run:
 // gulp scripts.
 // Uglifies and concat all JS files into one
-gulp.task( 'scripts', function() {
+gulp.task("scripts", function () {
 	var scripts = [
-		// Start - All BS4 stuff
-		paths.dev + '/js/bootstrap4/bootstrap.bundle.js',
-		paths.dev + '/js/themejs/*.js',
+		// Start - All Uikit stuff
+		// paths.dev + "/js/uikit/uikit.min.js",
+		// paths.dev + "/js/uikit/uikit-icons.min.js",
+		paths.dev + "/js/themejs/*.js",
 
 		// End - All BS4 stuff
 
-		paths.dev + '/js/skip-link-focus-fix.js',
+		paths.dev + "/js/skip-link-focus-fix.js",
 
 		// Adding currently empty javascript file to add on for your own themes´ customizations
 		// Please add any customizations to this .js file only!
-		paths.dev + '/js/custom-javascript.js',
+		paths.dev + "/js/custom-javascript.js",
 	];
 	gulp
-		.src( scripts, { allowEmpty: true } )
-		.pipe( babel( { presets: ['@babel/preset-env'] } ) )
-		.pipe( concat( 'theme.min.js' ) )
-		.pipe( uglify() )
-		.pipe( gulp.dest( paths.js ) );
+		.src(scripts, { allowEmpty: true })
+		.pipe(babel({ presets: ["@babel/preset-env"] }))
+		.pipe(concat("theme.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.js));
 
 	return gulp
-		.src( scripts, { allowEmpty: true } )
-		.pipe( babel() )
-		.pipe( concat( 'theme.js' ) )
-		.pipe( gulp.dest( paths.js ) );
-} );
+		.src(scripts, { allowEmpty: true })
+		.pipe(babel())
+		.pipe(concat("theme.js"))
+		.pipe(gulp.dest(paths.js));
+});
 
 // Deleting any file inside the /src folder
-gulp.task( 'clean-source', function() {
-	return del( [ 'src/**/*' ] );
-} );
+gulp.task("clean-source", function () {
+	return del(["src/**/*"]);
+});
 
 // Run:
 // gulp copy-assets.
 // Copy all needed dependency assets files from node_modules to theme's /js, /scss and /fonts folder. Run this task after npm update
 
 ////////////////// All Bootstrap SASS  Assets /////////////////////////
-gulp.task( 'copy-assets', function( done ) {
+gulp.task("copy-assets", function (done) {
 	////////////////// All Bootstrap 4 Assets /////////////////////////
 	// Copy all JS files
 	var stream = gulp
-		.src( paths.node + '/bootstrap/dist/js/**/*.js' )
-		.pipe( gulp.dest( paths.dev + '/js/bootstrap4' ) );
+		.src(paths.node + "/bootstrap/dist/js/**/*.js")
+		.pipe(gulp.dest(paths.dev + "/js/bootstrap4"));
 
 	// Copy all Bootstrap SCSS files
 	gulp
-		.src( paths.node + '/bootstrap/scss/**/*.scss' )
-		.pipe( gulp.dest( paths.dev + '/sass/bootstrap4' ) );
+		.src(paths.node + "/bootstrap/scss/**/*.scss")
+		.pipe(gulp.dest(paths.dev + "/sass/bootstrap4"));
 
 	////////////////// End Bootstrap 4 Assets /////////////////////////
 
 	// Copy all Font Awesome Fonts
 	gulp
-		.src( paths.node + '/font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}' )
-		.pipe( gulp.dest( paths.fonts ) );
+		.src(paths.node + "/font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}")
+		.pipe(gulp.dest(paths.fonts));
 
 	// Copy all Font Awesome SCSS files
 	gulp
-		.src( paths.node + '/font-awesome/scss/*.scss' )
-		.pipe( gulp.dest( paths.dev + '/sass/fontawesome' )	);
+		.src(paths.node + "/font-awesome/scss/*.scss")
+		.pipe(gulp.dest(paths.dev + "/sass/fontawesome"));
 
 	done();
-} );
+});
 
 // Deleting the files distributed by the copy-assets task
-gulp.task( 'clean-vendor-assets', function() {
-	return del( [
-		paths.dev + '/js/bootstrap4',
-		paths.dev + '/sass/bootstrap4',
-		paths.fonts + '/*wesome*.{ttf,woff,woff2,eot,svg}',
-		paths.dev + '/sass/fontawesome',
+gulp.task("clean-vendor-assets", function () {
+	return del([
+		paths.dev + "/js/bootstrap4",
+		paths.dev + "/sass/bootstrap4",
+		paths.fonts + "/*wesome*.{ttf,woff,woff2,eot,svg}",
+		paths.dev + "/sass/fontawesome",
 		paths.js + paths.vendor,
-	] );
-} );
+	]);
+});
 
 /**
  * Deletes all files inside the dist folder and the folder itself.
  *
  * Run: gulp clean-dist
  */
-gulp.task( 'clean-dist', function() {
-	return del( paths.dist );
-} );
+gulp.task("clean-dist", function () {
+	return del(paths.dist);
+});
 
 // Run
 // gulp dist
 // Copies the files to the dist folder for distribution as simple theme
 gulp.task(
-	'dist',
-	gulp.series( [ 'clean-dist' ], function() {
+	"dist",
+	gulp.series(["clean-dist"], function () {
 		return gulp
 			.src(
 				[
-					'**/*',
-					'!' + paths.node,
-					'!' + paths.node + '/**',
-					'!' + paths.dev,
-					'!' + paths.dev + '/**',
-					'!' + paths.dist,
-					'!' + paths.dist + '/**',
-					'!' + paths.distprod,
-					'!' + paths.distprod + '/**',
-					'!' + paths.sass,
-					'!' + paths.sass + '/**',
-					'!' + paths.composer,
-					'!' + paths.composer + '/**',
-					'!+(readme|README).+(txt|md)',
-					'!*.+(dist|json|js|lock|xml)',
-					'!CHANGELOG.md',
+					"**/*",
+					"!" + paths.node,
+					"!" + paths.node + "/**",
+					"!" + paths.dev,
+					"!" + paths.dev + "/**",
+					"!" + paths.dist,
+					"!" + paths.dist + "/**",
+					"!" + paths.distprod,
+					"!" + paths.distprod + "/**",
+					"!" + paths.sass,
+					"!" + paths.sass + "/**",
+					"!" + paths.composer,
+					"!" + paths.composer + "/**",
+					"!+(readme|README).+(txt|md)",
+					"!*.+(dist|json|js|lock|xml)",
+					"!CHANGELOG.md",
 				],
 				{ buffer: true }
 			)
-			.pipe( gulp.dest( paths.dist ) );
-	} )
+			.pipe(gulp.dest(paths.dist));
+	})
 );
 
 /**
@@ -301,38 +299,38 @@ gulp.task(
  *
  * Run: gulp clean-dist-product
  */
-gulp.task( 'clean-dist-product', function() {
-	return del( paths.distprod );
-} );
+gulp.task("clean-dist-product", function () {
+	return del(paths.distprod);
+});
 
 // Run
 // gulp dist-product
 // Copies the files to the /dist-prod folder for distribution as theme with all assets
 gulp.task(
-	'dist-product',
-	gulp.series( [ 'clean-dist-product' ], function() {
+	"dist-product",
+	gulp.series(["clean-dist-product"], function () {
 		return gulp
-			.src( [
-				'**/*',
-				'!' + paths.node,
-				'!' + paths.node + '/**',
-				'!' + paths.composer,
-				'!' + paths.composer + '/**',
-				'!' + paths.dist,
-				'!' + paths.dist + '/**',
-				'!' + paths.distprod,
-				'!' + paths.distprod + '/**',
-			] )
-			.pipe( gulp.dest( paths.distprod ) );
-	} )
+			.src([
+				"**/*",
+				"!" + paths.node,
+				"!" + paths.node + "/**",
+				"!" + paths.composer,
+				"!" + paths.composer + "/**",
+				"!" + paths.dist,
+				"!" + paths.dist + "/**",
+				"!" + paths.distprod,
+				"!" + paths.distprod + "/**",
+			])
+			.pipe(gulp.dest(paths.distprod));
+	})
 );
 
 // Run
 // gulp compile
 // Compiles the styles and scripts and runs the dist task
-gulp.task( 'compile', gulp.series( 'styles', 'scripts', 'dist' ) );
+gulp.task("compile", gulp.series("styles", "scripts", "dist"));
 
 // Run:
 // gulp
 // Starts watcher (default task)
-gulp.task( 'default', gulp.series( 'watch' ) );
+gulp.task("default", gulp.series("watch"));
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -8,81 +9,154 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-<?php do_action( 'wp_body_open' ); ?>
-<div class="site" id="page">
+	<?php do_action('wp_body_open'); ?>
+	<div class="site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar">
+		<!-- ******************* The Navbar Area ******************* -->
+		<div id="wrapper-navbar">
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+			<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e('Skip to content', 'understrap'); ?></a>
 
-		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
+			<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
 
-			<h2 id="main-nav-label" class="sr-only">
-				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-			</h2>
+				<h2 id="main-nav-label" class="sr-only">
+					<?php esc_html_e('Main Navigation', 'understrap'); ?>
+				</h2>
 
-		<?php if ( 'container' === $container ) : ?>
-			<div class="container">
-		<?php endif; ?>
+				<?php if ('container' === $container) : ?>
+					<div class="container">
+					<?php endif; ?>
 
 					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+					<?php if (!has_custom_logo()) { ?>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
+						<?php if (is_front_page() && is_home()) : ?>
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url(home_url('/')); ?>" itemprop="url"><?php bloginfo('name'); ?></a></h1>
 
 						<?php else : ?>
 
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" itemprop="url"><?php bloginfo('name'); ?></a>
 
 						<?php endif; ?>
 
-						<?php
+					<?php
 					} else {
 						the_custom_logo();
 					}
 					?>
 					<!-- end custom logo -->
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'understrap'); ?>">
+						<span class="navbar-toggler-icon"></span>
+					</button>
 
-				<!-- The WordPress Menu goes here -->
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				);
-				?>
-			<?php if ( 'container' === $container ) : ?>
-			</div><!-- .container -->
-			<?php endif; ?>
+					<!-- The WordPress Menu goes here -->
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location'  => 'primary',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'navbarNavDropdown',
+							'menu_class'      => 'navbar-nav ml-auto',
+							'fallback_cb'     => '',
+							'menu_id'         => 'main-menu',
+							'depth'           => 2,
+							'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+						)
+					);
+					?>
+					<?php if ('container' === $container) : ?>
+					</div><!-- .container -->
+				<?php endif; ?>
 
-		</nav><!-- .site-navigation -->
+			</nav><!-- .site-navigation -->
 
-	</div><!-- #wrapper-navbar end -->
+		</div><!-- #wrapper-navbar end -->
+
+
+
+		<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
+
+			<ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>1</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>2</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>3</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>4</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>5</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>6</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>7</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>8</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>9</h1>
+					</div>
+				</li>
+				<li>
+					<img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+					<div class="uk-position-center uk-panel">
+						<h1>10</h1>
+					</div>
+				</li>
+			</ul>
+
+			<a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+			<a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+		</div>
