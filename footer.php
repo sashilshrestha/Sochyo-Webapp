030ca1d7a0f881a82090f93e2efead262b215a54
sashilshrestha <sashilstha5@gmail.com>
2021-10-02 10:35:30 +0545

    Modifications Done

--- footer.php
+++ footer.php
@@ -15,12 +15,57 @@ defined('ABSPATH') || exit;
 ?>
 <?php get_template_part('sidebar-templates/sidebar', 'footerfull'); ?>
 
-<!-- <center>
-	<h5 class="uk-text-bolder uk-background-muted uk-padding-small" style="margin-top: 2rem;">Sochyo.com.np</h5>
-</center> -->
-
 
 </div><!-- #page we need this extra closing tag here -->
+
+<footer>
+	<div class="uk-container uk-container-large ss-footer-top">
+		<div class="uk-child-width-expand@s" uk-grid>
+			<div>
+				<div class="container left">
+					<div class="ss-logo">
+						<img src="<?php echo get_home_url(); ?>/wp-content/themes/sochyo-theme/img/logo.png" alt="">
+					</div>
+					<div class="ss-info">
+						<p><span uk-icon="location"></span>
+							New Baneshwor, Kathmandu Nepal</p>
+						<p><span uk-icon="phone"></span>
+							+977-984140545</p>
+						<p><span uk-icon="mail"></span>
+							sochyo@gmail.com</p>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div class="container middle">
+					<h3>Pay Via</h3>
+					<div class="ss-ewallet">
+						<img src="./wp-content/themes/sochyo-theme/img/landing-1.jpg" alt="">
+						<img src="./wp-content/themes/sochyo-theme/img/landing-1.jpg" alt="">
+						<img src="./wp-content/themes/sochyo-theme/img/landing-1.jpg" alt="">
+					</div>
+				</div>
+			</div>
+			<div>
+				<div class="container right">
+					<h3>Connect with us</h3>
+					<div class="ss-icons">
+						<a href=""><span uk-icon="facebook"></span></a>
+						<a href=""><span uk-icon="instagram"></span></a>
+						<a href=""><span uk-icon="linkedin"></span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="uk-container uk-container-large ss-footer-bottom">
+		<div class="ss-left">© All rights reserved.
+		</div>
+		<div class="ss-right">Development with love by: Sashil</div>
+
+	</div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 
